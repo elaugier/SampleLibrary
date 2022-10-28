ab8384fa260a776594c762454ce358825573c8e8
Eric Laugier <eric.laugier@egen.guru>
2022-10-28 22:33:22 +0200

    insert fake different treatments

--- SampleLibrary/MyWonderfullClass.cs
+++ SampleLibrary/MyWonderfullClass.cs
@@ -1,20 +1,41 @@
 ﻿using System;
+using RestSharp;
 
 namespace SampleLibrary
 {
     public class MyWonderfullClass
     {
+        private RestClient _restClient;
 
 #if NETCOREAPP3_1
-        private readonly string version = "3.1";
+        private readonly string _version = "3.1";
 #elif NET5_0
-        private readonly string version = "5.0";
+        private readonly string _version = "5.0";
 #else
-        private readonly string version = "6.0";
+        private readonly string _version = "6.0";
 #endif
-        public string getVersion()
+        MyWonderfullClass(Uri uri)
         {
-            return version;
+            _restClient = new RestClient(uri);
+        }
+        public string GetVersion()
+        {
+            return _version;
+        }
+
+        public RestClient GetRestClient()
+        {
+            return _restClient;
+        }
+
+        public async void DoSomething()
+        {
+#if NETCOREAPP3_1
+            var r = new RestRequest("/path", Method.Get);
+#else
+            var r = new RestRequest(new Uri("/path"), Method.Get);
+#endif
+            var result = await _restClient.ExecuteAsync(r);
         }
 
     }
