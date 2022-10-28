46e58090d300f4afa5f3e973318cb6edbf3f7e9b
Eric Laugier <eric.laugier@egen.guru>
2022-10-28 20:43:33 +0200

    Add project files.

---
+++ SampleLibrary/MyWonderfullClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    public class MyWonderfullClass
+    {
+
+#if NETCOREAPP3_1
+        private readonly string version = "3.1";
+#elif NET5_0
+        private readonly string version = "5.0";
+#else
+        private readonly string version = "6.0";
+#endif
+        public string getVersion()
+        {
+            return version;
+        }
+
+    }
+}
